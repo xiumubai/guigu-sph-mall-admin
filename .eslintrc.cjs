4258c0c7bba2e00a94efb5ebb8c5f8efa87c14ed
xiumubai <1547702880@qq.com>
2023-01-30 11:36:31 +0800

    feat: 配置eslint和prettier统一代码规范

---
+++ .eslintrc.cjs
@@ -0,0 +1,26 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+    jest: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:vue/vue3-essential',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended', // plugin-prettier
+    'plugin:vue/vue3-recommended', // plugin-vue
+  ],
+  overrides: [],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['vue', '@typescript-eslint'],
+  rules: {
+    'space-before-function-paren': ['error', 'never'],
+    semi: 0, // 结尾不要分号
+  },
+}
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,8 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
-
+function abstract() {
+  console.log(123)
+}
+abstract()
 createApp(App).mount('#app')
