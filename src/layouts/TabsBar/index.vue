5e6ecaa49814b1dea4a18e598519464265cdfea5
lihaibo@shanshu.ai <lihaibo@shanshu.ai>
2023-03-03 18:16:39 +0800

    feat: 增加tabsBar组件

--- src/layouts/TabsBar/index.vue
+++ src/layouts/TabsBar/index.vue
@@ -1,30 +1,208 @@
+<!--
+ * @Description: tabsBar 模块
+ * @Autor: 李海波
+ * @Date: 2023-03-02 17:50:49
+ * @LastEditors: gjzxlihaibo@163.com
+ * @LastEditTime: 2023-03-03 18:16:34
+-->
 <template>
-  <div class="tabs-bar-container">tabsbar</div>
+  <div class="m-tags-view">
+    <div class="tabs-view">
+      <el-tabs
+        v-model="activeTabsValue"
+        type="card"
+        @tab-click="tabClick"
+        @tab-remove="removeTab"
+      >
+        <el-tab-pane
+          v-for="item in visitedViews"
+          :key="item.path"
+          :path="item.path"
+          :label="item.title"
+          :name="item.path"
+          :closable="!(item.meta && item.meta.affix)"
+        >
+          <template #label>
+            <el-icon class="tabs-icon" v-if="item.meta && item.meta.icon">
+              <component :is="item.meta.icon"></component>
+            </el-icon>
+            {{ item.title }}
+          </template>
+        </el-tab-pane>
+      </el-tabs>
+    </div>
+    <div class="right-btn"></div>
+  </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
-
+import { computed, defineComponent, ref, onMounted, watch } from 'vue'
+import { useTabsBarStore } from '@/store/modules/tabsBar'
+import { useRoute, useRouter, RouteRecordRaw } from 'vue-router'
+import { TabsPaneContext, TabPaneName } from 'element-plus'
+import path from 'path-browserify'
+import { useAuthStore } from '@/store/modules/auth'
+import { routersType } from './types'
 export default defineComponent({
   setup() {
-    return {}
+    const tabsBarStore = useTabsBarStore()
+    const authStore = useAuthStore()
+    const routes = computed(() => authStore.authMenuList)
+    const visitedViews = computed<routersType[]>(
+      () => tabsBarStore.visitedViews,
+    )
+    const route = useRoute()
+    const router = useRouter()
+    let affixTags = ref<RouteRecordRaw[]>([])
+
+    // 添加当前路由
+    const addTags = () => {
+      const { name } = route
+      if (name === 'Login') {
+        return
+      }
+      if (name) {
+        tabsBarStore.addView(route as unknown as RouteRecordRaw)
+      }
+      return false
+    }
+
+    function filterAffixTags(routes: RouteRecordRaw[], basePath = '/') {
+      let tags = [] as any
+      routes.forEach((route) => {
+        if (route.meta && route.meta.affix) {
+          // 获取 path
+          const tagPath = path.resolve(basePath, route.path)
+          tags.push({
+            fullPath: tagPath,
+            path: tagPath,
+            name: route.name,
+            meta: { ...route.meta },
+          })
+        }
+        if (route.children) {
+          const tempTags = filterAffixTags(route.children, route.path)
+          if (tempTags.length >= 1) {
+            tags = [...tags, ...tempTags]
+          }
+        }
+      })
+      return tags
+    }
+
+    /**
+     * @description: 拿到需要固定的路由表，添加进 store
+     * @author: codeBo
+     */
+    const initTags = () => {
+      let routesNew = routes.value as RouteRecordRaw[]
+      let affixTag = (affixTags.value = filterAffixTags(routesNew))
+      for (const tag of affixTag) {
+        if (tag.name) {
+          tabsBarStore.addVisitedView(tag)
+        }
+      }
+    }
+
+    onMounted(() => {
+      initTags()
+      addTags()
+    })
+    watch(route, () => {
+      addTags()
+    })
+    const activeTabsValue = computed({
+      get: () => {
+        return tabsBarStore.activeTabsValue
+      },
+      set: (val) => {
+        tabsBarStore.setTabsMenuValue(val)
+      },
+    })
+
+    // 删除以后切换到下一个
+    function toLastView(activeTabPath: string) {
+      let index = visitedViews.value.findIndex(
+        (item) => item.path === activeTabPath,
+      )
+      const nextTab =
+        visitedViews.value[index + 1] || visitedViews.value[index - 1]
+      if (!nextTab) return
+      router.push(nextTab.path)
+      tabsBarStore.addVisitedView(nextTab)
+    }
+
+    // 点击事件
+    const tabClick = (tabItem: TabsPaneContext) => {
+      let path = tabItem.props.name as string
+      router.push(path)
+    }
+    const isActive = (path: string): boolean => {
+      return path === route.path
+    }
+    const removeTab = async (activeTabPath: TabPaneName): Promise<any> => {
+      if (isActive(activeTabPath as string)) {
+        toLastView(activeTabPath as string)
+      }
+      await tabsBarStore.delView(activeTabPath)
+    }
+    return {
+      activeTabsValue,
+      tabClick,
+      removeTab,
+      visitedViews,
+    }
   },
 })
 </script>
 
 <style scoped lang="scss">
-.tabs-bar-container {
+.m-tags-view {
   position: relative;
+  box-sizing: border-box;
   display: flex;
-  justify-content: space-between;
+  align-content: center;
   align-items: center;
+  justify-content: space-between;
+  height: $base-tabs-bar-height;
   padding-right: $base-padding;
   padding-left: $base-padding;
-  height: $base-tabs-bar-height;
-  background: $base-color-white;
-  box-sizing: border-box;
-  align-content: center;
   user-select: none;
+  background: $base-color-white;
   border-top: 1px solid #f6f6f6;
+
+  .right-btn {
+    flex-shrink: 0;
+    height: 100%;
+  }
+}
+
+.tags-view {
+  box-sizing: border-box;
+  flex: 1;
+  overflow: hidden;
+
+  .el-tabs--card :deep(.el-tabs__header) {
+    box-sizing: border-box;
+    height: 40px;
+    padding: 0 10px;
+    margin: 0;
+  }
+
+  :deep(.el-tabs) {
+    .el-tabs__nav {
+      border: none;
+    }
+
+    .el-tabs__header .el-tabs__item {
+      color: #ccc;
+      border: none;
+    }
+
+    .el-tabs__header .el-tabs__item.is-active {
+      color: $base-menu-background-active;
+      border-bottom: 2px solid $base-menu-background-active;
+    }
+  }
 }
 </style>
