0c7600d4f6b98a094d1ffaa314cd43449caa0bbb
xiumubai <1547702880@qq.com>
2023-03-10 16:21:32 +0800

    feat: change api baseurl

--- vite.config.ts
+++ vite.config.ts
@@ -45,7 +45,7 @@ export default defineConfig((config) => {
       port: Number(env.VITE_APP_PORT),
       proxy: {
         [env.VITE_APP_BASE_API]: {
-          target: 'http://sph-h5-api.atguigu.cn',
+          target: 'http://sph-api.atguigu.cn',
           changeOrigin: true,
           rewrite: (path) => path.replace(/^\/dev-api/, ''),
         },
